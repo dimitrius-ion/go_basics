d6b4f7fc18936c991bc9471884aa616302618be3
Dimitrius Ion <dimi@ion.local>
2024-01-25 10:01:00 -0800

    adding channels

--- goroutines/main.go
+++ goroutines/main.go
@@ -4,19 +4,33 @@ import (
 	"time"
 )
 
-func printMessage () {
-	for i := 0; i < 10; i++ {
-		fmt.Println("Hello World")
+func printMessage (chanel chan string) {
+	
+	for i := 0; i < 2; i++ {
+		fmt.Println("Hello")
 		time.Sleep(time.Second * 2)
 	}
+	chanel <- "Done"
 }
 
 // main runs in a goroutine
 func main() {
 
+	// create a channel that are used to communicate between goroutines
+	var chanel chan string
+	// chanel := make(chan string)
+	// buffer_channel := make(chan string, 2)
+
+
 	// go keyword is used to create a goroutine
-	go printMessage()
-	printMessage()
+	go printMessage(chanel)
+	response := <- chanel
+
+	// close the channel to avoid deadlock
+	close(chanel)
+	
+	fmt.Println(response)
+	time.Sleep(time.Second * 10)
 			
 	
 }
