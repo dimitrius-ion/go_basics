83e831d34ccd9c471828b903466a9edc7dcb38ce
Dimitrius Ion <dimi@ion.lan>
2024-01-30 07:08:03 -0800

    adding goroutines to fetch

--- crypto/main.go
+++ crypto/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/dimitrius-ion/go_basics/crypto/api"
 	"fmt"
+	"time"
 )
 
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	go getCurencyData("BTC")
+	go getCurencyData("ETH")
+	time.Sleep(5 * time.Second)
+}
+
+func getCurencyData(currency string) {
+	rate, err := api.GetRate(currency)
 	if err == nil {
 		fmt.Printf("Rate for %v: %.2f\n", rate.Currency, rate.Price)
 	}
