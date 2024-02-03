51716c7be486b68c45909a5ca7e238054e3d565c
Dimitrius Ion <dimi@ion.lan>
2024-02-03 06:51:37 -0800

    adding static server

--- FEMM/main.go
+++ FEMM/main.go
@@ -4,13 +4,21 @@ import (
 	"net/http"
 	"fmt"
 )
-
+func handlePing (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pong")
+}
 func main() {
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "pong")
-	})
-	
-	err := http.ListenAndServe(":8080", nil)
+	// create a new server
+	server := http.NewServeMux()
+	server.HandleFunc("/ping", handlePing)
+
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
+
+	// start the server
+	fmt.Println("Server started at http://localhost:8080")
+	defer fmt.Println("Server stopped")
+	err := http.ListenAndServe(":8080", server)
 	if err != nil {
 		fmt.Println(err)
 	}
