374ccfd213e60d59ada9eefea7ad98948302b32f
Dimitrius Ion <dimi@ion.lan>
2024-02-03 09:48:12 -0800

    adding api get endpoint args

--- FEMM/api/get.go
+++ FEMM/api/get.go
@@ -4,9 +4,24 @@ import (
 	"encoding/json"
 	"net/http"
 	"github.com/dimitrius-ion/go_basics/femm/data"
+	"strconv"
 )
 
 func Get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data.GetAll())
+
+	id := r.URL.Query().Get("id") // this is the same as the next line
+	// id := r.URL.Query()["id"]
+	
+	if id != "" {
+		index, err := strconv.Atoi(id)
+		print(index)
+		if err == nil && index >= 0 && index < len(data.GetAll()) {
+			json.NewEncoder(w).Encode(data.GetAll()[index])
+		}else{
+			http.Error(w, "Not Found", http.StatusNotFound)
+		}
+	}else{
+		json.NewEncoder(w).Encode(data.GetAll())
+	}
 }
