0179c23a388e4fa538f8200240b4dd4e42cd3ff8
Dimitrius Ion <dimi@ion.lan>
2024-02-03 07:42:26 -0800

    adding example of template

--- FEMM/main.go
+++ FEMM/main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"text/template"
 )
 func handlePing (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
 }
+
+func handleTemplate (w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("./templates/index.tmpl")
+	if err != nil {
+		fmt.Println(err)
+	}else{
+		html.Execute(w, "TEST VALUE")
+	}
+}
+
 func main() {
 	// create a new server
 	server := http.NewServeMux()
 	server.HandleFunc("/ping", handlePing)
+
+	server.HandleFunc("/template", handleTemplate)
 
 	fs := http.FileServer(http.Dir("./public"))
 	server.Handle("/", fs)
